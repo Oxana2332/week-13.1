a73fea620195cfb6930dd2687281b0016b190efd
Oxana2332 <far_north@ro.ru>
2021-12-30 20:57:18 +0300

    калькулятор

--- calc.js
+++ calc.js
@@ -1,5 +1,23 @@
 function calcSum(){
-let a = prompt("Введите первое число");
-let b = prompt("Введите второе число");
-return Number(a) + Number(b);
+    let a = prompt("Введите первое число");
+    let b = prompt("Введите второе число");
+    return Number(a) + Number(b);
+}
+
+function calcMinus(){
+    let a = prompt("Введите первое число");
+    let b = prompt("Введите второе число");
+    return a - b;
+}
+
+function calcMulti(){
+    let a = prompt("Введите первое число");
+    let b = prompt("Введите второе число");
+    return a * b;
+}
+
+function calcDivide(){
+    let a = prompt("Введите первое число");
+    let b = prompt("Введите второе число");
+    return a / b;
 }
